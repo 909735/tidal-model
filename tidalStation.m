b3afc99421c4016bb8e728f6978d60f17761ba92
909735 <94429523+909735@users.noreply.github.com>
2021-11-16 05:43:40 +0000

    Update tidalStation.m

--- tidalStation.m
+++ tidalStation.m
@@ -2,6 +2,8 @@ function [powerOutput] = tidalStation1D(phase,range)
 %TidalStation1D - 1D model of a single tidal station
 %   Testing git functionality
 %
+%   This is an edit to the file
+%
 %   Takes a site's tidal range and phase and converts it into a predicted
 %   power generation output.
 powerOutput = phase + range;
