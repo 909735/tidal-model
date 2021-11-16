d00dab7edf65f7c5b1839196aebcdf938eecbbec
909735 <94429523+909735@users.noreply.github.com>
2021-11-16 05:47:00 +0000

    Update tidalStation.m
    Testing branches

--- tidalStation.m
+++ tidalStation.m
@@ -2,6 +2,10 @@ function [powerOutput] = tidalStation1D(phase,range)
 %TidalStation1D - 1D model of a single tidal station
 %   Testing git functionality
 %
+%
+%
+%   This is a branch file
+%
 %   Takes a site's tidal range and phase and converts it into a predicted
 %   power generation output.
 powerOutput = phase + range;
