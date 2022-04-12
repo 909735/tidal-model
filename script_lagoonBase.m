2a07fd1ed7a238d750529f6ba92a920942e1539a
909735 <94429523+909735@users.noreply.github.com>
2022-04-12 21:50:16 +0100

    Add files via upload

--- script_lagoonBase.m
+++ script_lagoonBase.m
@@ -1,34 +1,6 @@
-%%  tidalStationReleaseWater
-%   Script that causes the lagoon height to change based on water flowing
-%   in or out.
+%%  script_lagoonBase
+%   Setup script to produce a blank lagoon height 
 
-%% Loop to release water at open points
-% For every release point:
-for x=[1:numData]
-    openInd = gateOpens(x);     % Current gate open index
-    lastLagH = lagH(openInd);        % Current gate open lagoon height
-    lastSeaH = seaH(openInd);        % Current gate open sea height
-    lagHO = lastLagH;
-    
-%   For each point within one cycle of the release point:
-    for x=[1:dayInd]
-        
-%       Set the indices and last values
-        curInd = openInd+x;
-        nextLagH = func_flow(lastLagH,lastSeaH);
-        
-%       if out of bounds
-        if curInd>numData, break
-        end
-            
-%       Set next point height
-        lagH(curInd) = nextLagH;
-        nextSeaH = seaH(curInd);
-        
-%       Set the calculated sea/lagoon height as previous
-        lastLagH = nextLagH; lastSeaH = nextSeaH;
-    end
-end
 
 
 
